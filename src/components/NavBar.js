07294e4107db3caa97a1f783af3554fa470f2ec0
Sabrina Cabaña <sabricaba@MacBook-Air-de-Sabrina.local>
2022-02-01 18:48:14 -0300

    Clase 3

---
+++ src/components/NavBar.js
@@ -0,0 +1,25 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import image from '../images/logo/1.png';
+
+const NavBar= () => {
+    return (
+        <header>
+            <a href="#"><img className="logoPrincipal" src={image} alt="" /></a>
+            <nav className="navTemporal">
+                    <ul>
+                        <li className="nav-item active">
+                        <a className="nav-link" aria-current="page" href="#">Adopta</a>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link" aria-current="page" href="#">Publica</a>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link" aria-current="page" href="#">Dona</a>
+                        </li>
+                    </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default NavBar;
